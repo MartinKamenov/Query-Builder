7b81b2a4aab1bbed4e921d2222c0d0959224ece4
MartinKamenov <martinkamenov10@gmail.com>
2019-11-28 11:19:55 +0200

    Add lazy loading query for fetching users

--- src/components/User/UserList.jsx
+++ src/components/User/UserList.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
+import { useLazyQuery } from 'react-apollo';
 import UserDetails from './UserDetails';
 
 const USERS_QUERY = gql`
@@ -15,33 +15,34 @@ const USERS_QUERY = gql`
 `;
 
 const UserList = () => {
+    const [runQuery, { called, loading, data }] = useLazyQuery(USERS_QUERY);
+    const handleClick = () => runQuery({ variables: {} });
+
+    const visualizeContent = useCallback(() => {
+        if(!called) {
+            return <h3>No users are loaded</h3>
+        }
+        if(loading) {
+            return (<h4>Loading...</h4>);
+        } else if(data.users) {
+            return (
+            <div className='row'>
+                {
+                    data.users.map((user) => (
+                    <UserDetails
+                        key={user['id']}
+                        user={user}/>
+                    ))
+                }
+            </div>);
+        }
+    }, [data, loading])
+
     return (
         <div style={{ textAlign: 'center', width: '100%' }}>
             <h1>Users</h1>
-            <Query query={USERS_QUERY}>
-                {
-                    ({ loading, error, data }) => {
-                        if(loading) {
-                            return <h4>Loading...</h4>
-                        }
-                        if(error) {
-                            console.log(error);
-                        }
-
-                        return (
-                            <div className='row'>
-                            {
-                                data.users.map((user) => (
-                                <UserDetails
-                                    key={user['id']}
-                                    user={user}/>
-                                ))
-                            }
-                            </div>
-                        );
-                    }
-                }
-            </Query>
+            <button className='btn btn-success' onClick={handleClick}>Get users</button>
+            { visualizeContent() }
         </div>
     );
 }
